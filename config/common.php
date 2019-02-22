76feaeeddbd972f540e62c34680ab4408be33552
Andrii Vasyliev <sol@hiqdev.com>
2019-02-22 14:17:58 +0200

    Making Yii 2/3 compatible config

--- config/common.php
+++ config/common.php
@@ -8,45 +8,33 @@
  * @copyright Copyright (c) 2017-2018, HiQDev (http://hiqdev.com/)
  */
 
-return [
-// Should not be used.
-//    'components' => [
-//        'autobus' => [
-//            'class' => \hiqdev\yii2\autobus\components\BranchedAutoBus::class,
-//        ],
-//        'commandBus' => [
-//            'class' => \hiqdev\yii2\autobus\components\CommandBusInterface::class,
-//            'middlewares' => [
-//                'load' => \hiqdev\yii2\autobus\bus\LoadMiddleware::class,
-//            ],
-//        ],
-//    ],
-    'container' => [
-        'definitions' => [
-            \hiqdev\yii2\autobus\components\CommandBusInterface::class => [
-                [
-                    '__class' => \hiqdev\yii2\autobus\components\TacticianCommandBus::class,
-                ],
-                [
-                    \yii\di\Instance::of(\my\CommandHandlerMiddleware::class),
-                ],
-            ],
-            \my\CommandHandlerMiddleware::class => [
-                [
-                    '__class' => \League\Tactician\Handler\CommandHandlerMiddleware::class,
-                ],
-                [
-                    \yii\di\Instance::of(\League\Tactician\Handler\CommandNameExtractor\ClassNameExtractor::class),
-                    \yii\di\Instance::of(\hiqdev\yii2\autobus\bus\NearbyHandlerLocator::class),
-                    \yii\di\Instance::of(\League\Tactician\Handler\MethodNameInflector\HandleInflector::class),
-                ],
-            ],
+$definitions = [
+    \hiqdev\yii2\autobus\components\CommandBusInterface::class => [
+        '__class' => \hiqdev\yii2\autobus\components\TacticianCommandBus::class,
+        '__construct()' => [
+            \yii\di\Reference::to(\my\CommandHandlerMiddleware::class),
         ],
-        'singletons' => [
-            \hiqdev\yii2\autobus\components\CommandFactoryInterface::class => \hiqdev\yii2\autobus\components\SimpleCommandFactory::class,
-            \hiqdev\yii2\autobus\components\AutoBusFactoryInterface::class => [
-                '__class' => \hiqdev\yii2\autobus\components\ContainerAutoBusFactory::class,
-            ],
+    ],
+    \my\CommandHandlerMiddleware::class => [
+        '__class' => \League\Tactician\Handler\CommandHandlerMiddleware::class,
+        '__construct()' => [
+            \yii\di\Reference::to(\League\Tactician\Handler\CommandNameExtractor\ClassNameExtractor::class),
+            \yii\di\Reference::to(\hiqdev\yii2\autobus\bus\NearbyHandlerLocator::class),
+            \yii\di\Reference::to(\League\Tactician\Handler\MethodNameInflector\HandleInflector::class),
         ],
     ],
 ];
+
+$singletons = [
+    \hiqdev\yii2\autobus\components\CommandFactoryInterface::class => [
+        '__class' => \hiqdev\yii2\autobus\components\SimpleCommandFactory::class,
+    ],
+    \hiqdev\yii2\autobus\components\AutoBusFactoryInterface::class => [
+        '__class' => \hiqdev\yii2\autobus\components\ContainerAutoBusFactory::class,
+    ],
+];
+
+return class_exists('Yii')
+    ? ['container' => ['definitions' => $definitions, 'singletons' => $singletons]]
+    : [array_merge($singletons, ['factory' => ['__construct' => ['definitions' => $definitions]]])]
+;
