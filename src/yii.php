86b2e5146e76a629bf17b52b3148a9cfbf5f02e0
Andrii Vasyliev <sol@hiqdev.com>
2019-05-13 12:19:22 +0300

    Fixed yii 2/3 compatibility

--- src/yii.php
+++ src/yii.php
@@ -5,6 +5,6 @@ namespace hiqdev\yii2\autobus;
 class yii {
     public static function referenceTo($id)
     {
-        return class_exists('Yii') ? \yii\di\Instance::of($id) : \yii\di\Reference::to($id);
+        return class_exists('yii\\di\\Reference') ? \yii\di\Reference::to($id) : \yii\di\Instance::of($id);
     }
 }
