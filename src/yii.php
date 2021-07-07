71e456f292bb739c0a5d404bb60f7f9fdc2d114e
strorch <smy980807@ukr.net>
2021-07-07 18:00:50 +0300

    fixed to new namespaces

--- src/yii.php
+++ src/yii.php
@@ -5,6 +5,8 @@ namespace hiqdev\yii2\autobus;
 class yii {
     public static function referenceTo($id)
     {
-        return class_exists('yii\\di\\Reference') ? \yii\di\Reference::to($id) : \yii\di\Instance::of($id);
+        return class_exists(\Yiisoft\Factory\Definition\Reference::class)
+            ? \Yiisoft\Factory\Definition\Reference::to($id)
+            : \yii\di\Instance::of($id);
     }
 }
